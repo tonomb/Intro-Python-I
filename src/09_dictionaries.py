5f21e2052832eddeee8b37756b84602e4d3646d1
antonio martinez baez <tono.mtzb@gmail.com>
2020-08-31 17:17:39 -0500

    completed 10

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,14 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({
+    "lat": 42,
+    "lon": -123,
+    "name": "added place"
+
+})
+
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +50,14 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]["name"] = "not a real place"
+waypoints[0]["lon"] = -130
+print(waypoints[0])
 
+print()
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for place in waypoints:
+    for field in place:
+        print(field, place[field])
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,25 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+print(is_even(4))
+print(is_even(12345))
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def even(num):
+    if num % 2 == 0:
+        print('Even')
+    else:
+        print('Odd')
 
+even(num)
